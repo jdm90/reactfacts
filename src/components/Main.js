387bc290a27c74ee584428a3a24d5158a6c101f6
Justin Miles <jdmiles90@gmail.com>
2021-12-08 12:19:59 -0600

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Main() {
+  return (
+    <main>
+      <h1 className="main-title">Fun facts about React</h1>
+      <ul className="main-facts">
+        <li>React.js is a JavaScript Libray</li>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>The main purpose of React is to be fast, scalable, and simple</li>
+        <li>Utilizes a virtual DOM</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+        <li>Used by large scale social media sites such as Instagram, Facebook, and Reddit for their front-end website development needs</li>
+        <li>Used by New York Times, Netflix, and SalesForce for the development of their mobile app</li>
+      </ul>
+    </main>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="../images/react-icon-small.png" alt="" className="nav-icon" />
+      <h3 className="nav-logo-text">ReactFacts</h3>
+      <h4 className="nav-title">Created by m0rph3us</h4>
+    </nav>
+  );
+}
